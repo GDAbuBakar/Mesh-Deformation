f05f71000a8814aebcda106c236f8aa08c079991
GDAbubakar <gameDevExtended@gmail.com>
2021-05-30 14:46:29 +0500

    Initial commit

---
+++ Assets/Scripts/SliceMeshes-master/SliceListener.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SliceListener : MonoBehaviour
+{
+    public Slicer slicer;
+
+    private Vector3 oldPos;
+    private Vector3 curPos;
+    private Vector3 orgDir;
+    private Vector3 curDir;
+    private Transform collTransform;
+    private bool isEntered;
+    private bool canCut = true;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //slicer.isTouched = true;
+
+        collTransform = other.transform;
+
+        oldPos = transform.position;
+        isEntered = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        curPos = transform.position;
+
+        if(isEntered)
+        {
+            if (curPos != oldPos)
+            {
+                orgDir = curPos - oldPos;
+                orgDir.Normalize();
+                isEntered = false;
+            }
+        }
+
+        if (curPos.y > oldPos.y || curPos.x < oldPos.x)
+            canCut = false;
+
+        curDir = (curPos - oldPos).normalized;
+
+        if (curDir != orgDir)
+            canCut = false;
+
+        //print($"{orgDir}, {curDir}");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (transform.position.y > oldPos.y || transform.position.x < oldPos.x)
+            canCut = false;
+
+        if (canCut)
+        {
+            slicer.isTouched = true;
+            print("done");
+        }
+
+        orgDir = new Vector3();
+        curDir = new Vector3();
+
+        canCut = true;
+    }
+}
---
+++ Assets/Scripts/SliceMeshes-master/Slicer.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using EzySlice;
+public class Slicer : MonoBehaviour
+{
+    public Material materialAfterSlice;
+    public LayerMask sliceMask;
+    public bool isTouched;
+
+    private readonly string hullName = "Lower_Hull";
+
+    private void Update()
+    {
+        if (isTouched == true)
+        {
+            isTouched = false;
+
+            Collider[] objectsToBeSliced = Physics.OverlapBox(transform.position, new Vector3(1, 0.1f, 0.1f), transform.rotation, sliceMask);
+            print(objectsToBeSliced.Length);
+
+            foreach (Collider objectToBeSliced in objectsToBeSliced)
+            {
+                SlicedHull slicedObject = SliceObject(objectToBeSliced.gameObject, materialAfterSlice);
+
+                //if (slicedObject != null)
+                //{
+                    GameObject upperHullGameobject = slicedObject.CreateUpperHull(objectToBeSliced.gameObject, materialAfterSlice);
+                    GameObject lowerHullGameobject = slicedObject.CreateLowerHull(objectToBeSliced.gameObject, materialAfterSlice);
+
+                    upperHullGameobject.transform.position = objectToBeSliced.transform.position;
+                    lowerHullGameobject.transform.position = objectToBeSliced.transform.position;
+
+                    MakeItPhysical(upperHullGameobject);
+                    MakeItPhysical(lowerHullGameobject);
+
+                    Destroy(objectToBeSliced.gameObject);
+                //}
+            }
+        }
+    }
+
+    private void MakeItPhysical(GameObject obj)
+    {
+        obj.AddComponent<MeshCollider>().convex = true;
+        Rigidbody rb = obj.AddComponent<Rigidbody>();
+
+        if (obj.name == hullName)
+        {
+            rb.isKinematic = true;
+        }
+        else
+        { 
+            rb.interpolation = RigidbodyInterpolation.Interpolate;
+            rb.AddExplosionForce(100, rb.transform.position, 20);
+        }
+
+
+        obj.layer = 8;
+    }
+
+    private SlicedHull SliceObject(GameObject obj, Material crossSectionMaterial = null)
+    {
+        return obj.Slice(transform.position, transform.up, crossSectionMaterial);
+    }
+}
